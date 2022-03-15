8d2fa9b557d01f69aa66c5b4eb5c448ebaae10c3
KeichiTS <kkeichi@gmail.co>
2022-03-15 01:08:58 -0300

    changes

--- Scripts/Map_generator.gd
+++ Scripts/Map_generator.gd
@@ -1,54 +1,80 @@
 extends Node2D
 
+var map_a = preload("res://Maps/map_a.tscn")
+var map_b = preload("res://Maps/map_b.tscn")
+var map_c = preload("res://Maps/map_c.tscn")
+var map_d = preload("res://Maps/map_g.tscn")
+var map_e = preload("res://Maps/map_e.tscn")
+var map_f = preload("res://Maps/map_f.tscn")
+var map_g = preload("res://Maps/map_g.tscn")
 
-var map_lobby = [preload("res://Maps/map_a.tscn"),preload("res://Maps/map_b.tscn"),preload("res://Maps/map_c.tscn")]
-var map_a = [preload("res://Maps/map_a.tscn"),preload("res://Maps/map_b.tscn"),preload("res://Maps/map_c.tscn")]
-var map_b = [preload("res://Maps/map_d.tscn"),preload("res://Maps/map_e.tscn")]
-var map_c = [preload("res://Maps/map_f.tscn"),preload("res://Maps/map_g.tscn")]
-var map_d = [preload("res://Maps/map_a.tscn"),preload("res://Maps/map_b.tscn"),preload("res://Maps/map_c.tscn")]
-var map_e = [preload("res://Maps/map_d.tscn"),preload("res://Maps/map_e.tscn")]
-var map_f = [preload("res://Maps/map_a.tscn"),preload("res://Maps/map_b.tscn"),preload("res://Maps/map_c.tscn")]
-var map_g = [preload("res://Maps/map_f.tscn"),preload("res://Maps/map_g.tscn")]
-
-var mapgen = 0
-var nextgen = 0
-var pos_x = 0
-var pos_y = 0 
 
 
 func _ready():
 	randomize()
-	for i in range(10+1):
-		if mapgen == 0: 
-			pos_x += 1024
-		elif mapgen == 1:
-			pos_y -= 600
-		elif mapgen == 2: 
-			pos_y += 600
-			
-		mapgen = map_generator(mapgen,pos_x,pos_y)
+	map_generator()
 		
-func map_generator(n,pos_x,pos_y):
-	if n == 0:
-		var index = randi() % 3
-		var map = map_a[index].instance()
-		map.global_position = global_position + Vector2(pos_x ,pos_y)
-		add_child(map)
-		
-		return index
-		
-	elif n == 1:
-		var index = randi() % 2
-		var map = map_b[index].instance()
-		map.global_position = global_position + Vector2(pos_x,pos_y)
-		add_child(map)
-
-		return index
-
-	elif n == 2:
-		var index = randi() % 2
-		var map = map_c[index].instance()
-		map.global_position = global_position + Vector2(pos_x,pos_y)
-		add_child(map)
+func map_generator():
+	var index 
+	var mapgen = 0
+	var pos_x = 0
+	var pos_y = 0 
+	var map
 	
-		return index
+	for i in range(10+1):
+		print(mapgen)
+		if mapgen == 0 or mapgen == 3 or mapgen == 5:
+			index = randi() % 3
+			pos_x += 1024
+			if index == 0:
+				map = map_a.instance()
+				map.global_position = global_position + Vector2(pos_x ,pos_y)
+				mapgen = 0
+				add_child(map)
+			
+			elif index == 1: 
+				map = map_b.instance()
+				map.global_position = global_position + Vector2(pos_x ,pos_y)
+				mapgen = 1
+				add_child(map)
+				
+			elif index == 2:
+				map = map_c.instance()
+				map.global_position = global_position + Vector2(pos_x ,pos_y)
+				mapgen = 2
+				add_child(map)
+		
+		elif mapgen == 1 or mapgen == 4: 
+			pos_y -= 600
+			index = randi() % 2
+			if index == 0: 
+				map = map_d.instance()
+				map.global_position = global_position + Vector2(pos_x, pos_y)
+				mapgen = 3
+			elif index == 1: 
+				map = map_e.instance()
+				map.global_position = global_position + Vector2(pos_x, pos_y)
+				mapgen = 4
+				add_child(map)
+				
+		elif mapgen == 2 or mapgen == 6: 
+			pos_y += 600
+			index = randi() % 2
+			if index == 0: 
+				map = map_f.instance()
+				map.global_position = global_position + Vector2(pos_x, pos_y)
+				mapgen = 5
+				add_child(map)
+				
+			elif index == 1: 
+				map = map_g.instance()
+				map.global_position = global_position + Vector2(pos_x, pos_y)
+				mapgen = 6
+				add_child(map)
+
+		
+	
+	
+	
+	
+	
